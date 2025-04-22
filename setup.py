c85c820346c7acbc156c1aa63876613461b02072
Israel Ferreira <israelvf@gmail.com>
2025-04-22 15:57:27 -0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     keywords='TeX, LaTeX, Markdown, HTML markdown2html, latex2mathml',
     packages=find_packages(),
     python_requires='>=3.7, <4',
-    install_requires=['markdown', 'latex2mathml'],
+    install_requires=['markdown2', 'latex2mathml'],
     project_urls={
-        'Bug Reports': 'https://github.com/polarwinkel/mdtex2html/issues',
-        'Source': 'https://github.com/polarwinkel/mdtex2html',
+        'Bug Reports': 'https://github.com/israelvf/mdtex2html/issues',
+        'Source': 'https://github.com/israelvf/mdtex2html',
     },
 )
